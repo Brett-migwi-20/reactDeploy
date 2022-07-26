b98abbfcd291a314628ededd17643056e1303be1
Brett <brettnjeri21@gmail.com>
2022-07-26 04:02:35 +0100

    Updates

---
+++ precache-manifest.7598894ae4ddbfba832b287ed7ed4411.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d583409be5f54181927f",
+    "url": "/reactDeploy/static/css/main.7d1ac3fe.chunk.css"
+  },
+  {
+    "revision": "d583409be5f54181927f",
+    "url": "/reactDeploy/static/js/main.2b70392e.chunk.js"
+  },
+  {
+    "revision": "3d66b45fc5e8a5b9b0e4",
+    "url": "/reactDeploy/static/js/runtime~main.a31f273e.js"
+  },
+  {
+    "revision": "e801277ca269fb06fb33",
+    "url": "/reactDeploy/static/css/2.19128409.chunk.css"
+  },
+  {
+    "revision": "e801277ca269fb06fb33",
+    "url": "/reactDeploy/static/js/2.86d16291.chunk.js"
+  },
+  {
+    "revision": "642b6c12e7d0ba945a6975e9b5c75e47",
+    "url": "/reactDeploy/static/media/login-page-pic.642b6c12.png"
+  },
+  {
+    "revision": "a51e0410fb74d1e6a5ce698f05b334b1",
+    "url": "/reactDeploy/index.html"
+  }
+];
--- src/contexts/AuthContext.js
+++
@@ -1,8 +0,0 @@
-import React, { useContext, useState, useEffect } from "react";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
-const AuthContext = React.createContext();
-
-export function AuthProvider({ children }) {
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
-}
